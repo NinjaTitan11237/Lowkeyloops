b6d17bdb689c1ffc2bc1ddcbada95c9a9675894b
NinjaTitan11237 <muthu11.krishna@gmail.com>
2026-04-28 12:42:01 +0530

    Delete Data.js

--- Data.js
+++
@@ -1,5 +0,0 @@
-export const products = [
-{ id:1,name:"Bear Plushie",price:799,category:"plushies",image:"https://i.imgur.com/Vz81GEl.jpg",stock:5 },
-{ id:2,name:"Crochet Bag",price:1299,category:"accessories",image:"https://i.imgur.com/9XnK8VY.jpg",stock:2 },
-{ id:3,name:"Sweater",price:1999,category:"wearables",image:"https://i.imgur.com/3ZQ3Z4E.jpg",stock:0 }
-];
